2edf0b3bf4cd75566e743ea31958ed0233017de3
hadnett <williamsrhadnett@outlook.com>
2020-12-28 20:00:22 +0000

    Save dataset to CSV file for future use
    Scraped all data required from website including fake and real
    news stories.
    
    Exported that data to CSV file.
    
    Can now read that data from a CSV file for future use.
    
    Added an if statement to let the user toggle whether or not they
    want to scrape the website for new content.

--- DS_Project_William_Hadnett.py
+++ DS_Project_William_Hadnett.py
@@ -10,6 +10,11 @@
 
 from bs4 import BeautifulSoup
 import requests
+import time
+import pandas as pd
+import os
+
+os.chdir('/Users/williamhadnett/Documents/Data_Science/Data_Science_Project_William_Hadnett')
 
 # Discovered a way to find elements in the HTML tree via Beautiful Soup's 
 # find() function https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find.
@@ -24,12 +29,51 @@ import requests
 # while also reducing code complexity and making the code more readable.  
 
 post_details = []
-for i in range(1,2,1):
-    url= 'https://www.politifact.com/factchecks/list/?page='+str(i)+'&ruling=true'
-    html = requests.get(url).text
-    soup = BeautifulSoup(html,	'html5lib')
-    soup = soup.find_all('li',class_ = 'o-listicle__item')
-    for post in soup:
-        quote = post.find("div", class_ = 'm-statement__quote').text.strip()
-        author = post.find("a", class_ = 'm-statement__name').text.strip()
-        post_details.append([author, quote])
+scrapeData = False
+
+def parseHTML(isTrue):
+    for i in range(1,25,1):
+        url= 'https://www.politifact.com/factchecks/list/?page='+str(i)+'&ruling='+ isTrue +''
+        html = requests.get(url).text
+        soup = BeautifulSoup(html,	'html5lib')
+        soup = soup.find_all('li',class_ = 'o-listicle__item')
+        for post in soup:
+            quote = post.find("div", class_ = 'm-statement__quote').text.strip()
+            source = post.find("a", class_ = 'm-statement__name').text.strip()
+            date = post.find("div", class_ = 'm-statement__desc').text.strip()
+            post_details.append([source, quote, date, isTrue])
+        time.sleep(2)
+
+if scrapeData:
+    # Get True News Stories
+    parseHTML('true')
+    
+    # Get Fake News Stories
+    parseHTML('false')
+    
+    # 1440 posts retrieved.
+    
+    df = pd.DataFrame(post_details, columns=['Source', 'Quote', 'Date', 'isTrue'])
+    
+    # Write data to CSV file for future use. 
+    df.to_csv('fake_news.csv', encoding="utf-8", index=False)
+else:
+    df = pd.read_csv('fake_news.csv')
+
+
+# TODO Data Cleaning Remove Data String. 
+# TODO Also Determine if Person is male or female.
+# TODO Setiment Analysis of posts.
+# TODO Export Results to CSV file.
+# TODO Shuffle Data. 
+
+
+
+
+
+
+
+
+
+
+
